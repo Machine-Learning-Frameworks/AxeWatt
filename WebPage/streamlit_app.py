2c5cf8423784ade52751c6ca3255547a0233e44c
Francisco Gandala <96669523+FGandala@users.noreply.github.com>
2023-07-24 10:31:38 -0300

    Update streamlit_app.py

--- WebPage/streamlit_app.py
+++ WebPage/streamlit_app.py
@@ -153,6 +153,6 @@ def home():
     if st_mapa['last_active_drawing']:
      st.session_state['estado_escolhido'] = st_mapa['last_active_drawing']['properties']['NOME2']
 
-    st.subheader("Gráfico de Demanda " + st.session_state['estado_escolhido'])
+    st.subheader("Gráfico de demanda " + st.session_state['estado_escolhido'])
     st.altair_chart(cria_grafico_consumo(filtra_dados(st.session_state['estado_escolhido'],ano_inicial,ano_final)), theme="streamlit", use_container_width=True)
 home()
